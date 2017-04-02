c5f2552b34e948df0d20bb4ab1d62dcbf008129d
janekptacijarabaci <janekptacijarabaci@seznam.cz>
2017-04-02 10:14:48 +0200

    Style clean up

--- modules/remoteScript.js
+++ modules/remoteScript.js
@@ -450,10 +450,10 @@ RemoteScript.prototype.install = function (aOldScript, aOnlyDependencies) {
 
     this.script.setFilename(this._baseName, this._scriptFile.leafName);
     /*
+    Part 3/3 (install.js - Part 1/3, remoteScript.js - Part 2/3)?
     See #1919.
     Sometimes - throws an errors:
-      NS_ERROR_FILE_IS_LOCKED:
-        Component returned failure code:
+      NS_ERROR_FILE_IS_LOCKED: Component returned failure code:
         0x8052000e (NS_ERROR_FILE_IS_LOCKED) [nsIFile.moveTo]
         remoteScript.js
     */
